b660e7d0d376736376553a535f2880e7c74b8713
EXLUZIV <pegasfasmain@gmail.com>
2020-05-26 00:08:42 +0300

    Add clear,when click button

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
         $("#form_error_message_frontend + div > div:last-child label").addClass("last");
     })
 $("#cout").click(function (e){
+  document.getElementById('out').value = "";
   var text = ("input[name*='name']");
   var a = $("input[name*='in']");
   var b = $("input[name*='after']");
