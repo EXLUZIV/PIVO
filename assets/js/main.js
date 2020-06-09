864d356fccd63a15de045fa05f16f2eefaf58e59
EXLUZIV <pegasfasmain@gmail.com>
2020-06-09 10:59:06 +0300

    change a=first_number, b=last_number,c=rate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,35 @@
 $(document).ready(function(){
+
+        
         $("#form_error_message_frontend + div > div:last-child label").addClass("last");
-    })
-$("#cout").click(function (e){
+
+
+        $("#cout").click(function (e){
   document.getElementById('out').value = "";
-  var text = ("input[name*='name']");
-  var a = $("input[name*='in']");
-  var b = $("input[name*='after']");
-  var c = $("input[name*='rate']");
-  if(b>a){
-    b = (b - a)* c;
-      $('#out').append(b);
+
+
+  var nameText = ("input[name*='nameText]").val();
+  var nameFloat = parseFloat(nameText);
+
+  var first_numbersText = $("input[name*='first_numbersText']").val();
+  var first_numbersText = parseFloat(first_numbersText);
+
+  var last_numbersText = $("input[name*='last_numbersText']").val();
+  var last_numbersFloat = parseFloat(last_numbersText);
+
+  var rateText = $("input[name*='rateText']").val();
+  var rateFloat = parseFloat(rateText);
+
+  console.log(name);
+
+  if(last_numbers_Float>first_numbersFloat){
+    last_numbersFloat = (last_numbersFloat - first_numbersFloat)* rateFloat;
+      $('#out').append(last_numbersFloat);
   }
   else{
     $('#out').append("Введите правильные числа");
   }
 });   
+
+
+    })
